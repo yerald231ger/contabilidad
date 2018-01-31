5a7ceb44975d722fd4d36a1ae4b7111481689938
Gerardo Sánchez Hernández <yerald231ger@live.com>
2018-01-30 18:01:08 -0600

    clase roles

---
+++ data/Objetos/Roles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace datos.Objetos
+{
+    public class Role 
+    {
+        public int IdRole { get; set; }
+        public string Nombre { get; set; }
+    }
+}
--- data/Objetos/Usuario.cs
+++ data/Objetos/Usuario.cs
@@ -6,5 +6,13 @@ namespace datos.Objetos
 {
     public class Usuario : Base<int>
     {
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public DateTime FechaNacimineto { get; set; }
+        public string Email { get; set; }
+        public string NumeroCelular { get; set; }
+
+        public ICollection<Role> Roles { get; set; }
     }
 }
